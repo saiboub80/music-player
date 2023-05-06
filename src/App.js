c4985459e64efbc7157faeb360868b2e60e791dd
Saibou <saiboub80@gmail.com>
2023-05-06 13:25:31 -0400

    4-fetch-data-with-useeffect-part-1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
-import { useEffect, useState } from 'react'
+import { useState,useEffect } from 'react'
 import Gallery from './Components/Gallery'
 import SearchBar from './Components/SearchBar'
 
-function App(){
-    const [search, setSearch] = useState('')
-    const [message, setMessage] = useState('Search for Music!')
-    const [data, setData] = useState([])
 
-    useEffect(() => {
-      const fetchData = async () => {
-          document.title = `${search} Music`
-          const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
-          const resData = await response.json()
-          if (resData.results.length > 0) {
-              setData(resData.results)
-          } else {
-              setMessage('Not Found')
+const App = () => {
+  let [search, setSearch] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState([])
+
+  const API_URL = 'https://itunes.apple.com/search?term='
+
+  useEffect(() => {
+      if(search) {
+          const fetchData = async () => {
+              document.title = `${search} music`
+              const response = await fetch(API_URL + search)
+              const resData = await response.json()
+              if (resData.results.length > 0) {
+                  return setData(resData.results)
+              } else {
+                  return setMessage('Not Found.')
+              }
           }
+          fetchData()
       }
-      fetchData()
   }, [search])
-  
 
-    return (
-        <div>
-            <SearchBar />
-            {message}
-            <Gallery />
-        </div>
-    )
+  const handleSearch = (e, term) => {
+      e.preventDefault()
+      setSearch(term)
+  }
+
+  return (
+      <div>
+          <SearchBar handleSearch={handleSearch} />
+          {message}
+          <Gallery data={data} />
+      </div>
+  )
 }
 
 export default App
+
 
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,14 +1,18 @@
-        import { useState } from 'react'
-        function SearchBar(props) {
-            const [searchTerm, setSearchTerm] = useState('')
+function SearchBar(props){
+    // We can comment out our searchTerm state variable as we are not using it!
+    // let [searchTerm, setSearchTerm] = useState('')
 
-            return (
-                <form>
-                    <input type="text" placeholder="Enter a search term here" />
-                    <input type="submit" />
-                </form>
-            )
-        }
+    return (
+            <form>
+                <input type="text" placeholder="Search Here"
+                    onChange={
+                        (e) => props.handleSearch(e, e.target.value)
+                    } />
+                <input type="submit" />
+            </form>
+    )
+}
 
-        export default SearchBar
+export default SearchBar
+
     
